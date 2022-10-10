aded5b1122ce63e39000d32a35afaafdc1502e55
jack41402 <lucky368368@gmail.com>
2022-10-10 17:42:54 +0800

    assignment update

--- PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 2 Quadratic Equation Solver/assgn2_D1166506/assgn2_D1166506.c
+++ PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 2 Quadratic Equation Solver/assgn2_D1166506/assgn2_D1166506.c
@@ -4,7 +4,7 @@
 int main()
 {
     //Declare the required integer variables a, b, and c, and flag to record the status.
-    int a , b , c , flag ;
+    double a , b , c ;
     //Print as required.
     printf ("Solving roots of equation a*X^2+b*X+c = 0.\n\n") ;
     //Use do-while to prevent the user from mistyping the non-integer variables.
@@ -14,11 +14,11 @@ int main()
         printf ("Please enter three integer coefficients a, b, and c: ") , fflush(stdout) ;
         //Clear the buffer in order to avoid the scanf function getting the previous false input.
         fflush(stdin) ;
-        //Get the variables a, b, and c, and use a variable flag to record the number of inputs correctly.
-        flag = scanf ("%d %d %d" , &a , &b , &c) ;
+        //Get the variables a, b, and c.
+        scanf ("%lf %lf %lf" , &a , &b , &c) ;
     }
-    //If the number of inputs correctly isn't equal to 3 or a is equal to 0, input the variables a, b, and c again.
-    while (flag!=3 || a==0);
+    //If variable a, b, or c is not an integer value or variable a is equal to 0, input the variables a, b, and c again.
+    while (a!=(int)a || b!=(int)b || c!=(int)c || a==0);
     //Declare and initialize the value of the variable D, a discriminant of the quadratic equation.
     double D=(b*b-4*a*c) ;
     //Use conditional expressions to determine and print the equation.
@@ -27,14 +27,14 @@ int main()
     else printf ("\nThe complex roots of equation ") ;
     if (a==1) printf ("X**2") ;
     else if (a==-1) printf ("-X**2") ;
-    else if (a!=0) printf ("%dX**2" , a) ;
+    else if (a!=0) printf ("%.0lfX**2" , a) ;
     if (b==1) printf ("+X") ;
     else if (b==-1) printf ("-X") ;
-    else if (b!=0) printf ("%+dX" , b) ;
-    if (c) printf ("%+d" , c) ;
+    else if (b!=0) printf ("%+.0lfX" , b) ;
+    if (c) printf ("%+.0lf" , c) ;
     printf ("=0 ") ;
     //Separate the root into two parts, and combine them in different ways according to the value of variable D.
-    double x1 = -b/(2*(double)a) , x2 = sqrt(fabs(D))/(2*(double)a) ;
+    double x1 = -b/(2*a) , x2 = sqrt(fabs(D))/(2*a) ;
     //Use conditional expressions to determine and print the equation.
     if (D==0) printf ("is %.4lf.\n" , x1+x2) ;
     else if (D>0) printf ("are %.4lf and %.4lf.\n" , x1+x2 , x1-x2) ;
