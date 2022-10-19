1893bc0b0d9b4106d58fa01e89b1c0e84baf5a06
jack41402 <lucky368368@gmail.com>
2022-10-20 03:19:30 +0800

    update

--- PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 3 Binary Adder-Subtractor/assgn3_D1166506/assgn3_D1166506.c
+++ PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 3 Binary Adder-Subtractor/assgn3_D1166506/assgn3_D1166506.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
-//The output will print X, Y, and S in both decimal and binary format.
-void printBinary(unsigned int n)
+void printBinary(unsigned int n) // Print the integer variable in binary.
 {
-    for (int i=1 ; i<=32 ; i++) // Output all 32 bits.
+    for (int i=1 ; i<=32 ; i++) // Output all 32 bits of the variable in binary.
     {
-        printf ("%d" , (n>>(32-i))&1) ; // Output X bit of n from left to right.
-        if (i%4==0) printf (" ") ; // Print X space every four bits.
+        printf ("%d" , (n>>(32-i))&1) ; // Output bits of n from left to right.
+        if (i%4==0) printf (" ") ; // Print space every four bits.
     }
 }
 
-void adder (int X , int Y , int *carry , int *ans)
+void adder (int X , int Y , int *carry , int *ans) // full adder
 {
     for (int i=0 ; i<32 ; ++i)
     {
-        //Get the bit of integer variables X and Y ;
-        *ans |= ((((X>>i) & 1 ^ (Y>>i) & 1) ^ *carry)<<i) ;
-        if (i==31) *carry ^= ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry));
-        else *carry = ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry)) ;
+        *ans |= ((((X>>i) & 1 ^ (Y>>i) & 1) ^ *carry)<<i) ; // Use Leftshift and OR to add the C_in^(X^Y) to the i-th digit.
+        if (i==31) *carry ^= ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry)); // Use XOR to catch the flag of overflow. The sum of two positive or negative numbers won't be negative or positive.
+        else *carry = ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry)) ; // To determine whether to carry in a normal situation, one calculates ((X & Y)|(XY) & carry).
     }
 }
 
@@ -28,12 +26,11 @@ int main()
         int X , Y , carry=0 , ans=0 ;
         char operator ;
         scanf("%d %c %d", &X, &operator, &Y) ;
-        if (X==0 && Y==0) break ;
-        if (operator == '-') carry = 1 ;
-        adder(X , (operator=='-') ? ~Y : Y , &carry , &ans) ;
-        //Use carry to catch the flag
-        if (carry) printf("Overflow!\n");
-        else
+        if (X==0 && Y==0) break ; // Break the loop while X and Y are both equal to 0.
+        if (operator == '-') carry = 1 ; // Carry in at the first digit while the operator is negative.
+        adder(X , (operator=='-') ? ~Y : Y , &carry , &ans) ; // Use the function adder to sum up X and Y.
+        if (carry) printf("Overflow!\n");  // Use carry to catch the flag
+        else // print the variables X, Y, and S in both decimal and binary.
         {
             printf ("X = %-10d  Binary value: " , X) , printBinary (X) , printf ("\n") ;
             printf ("Y = %-10d  Binary value: " , Y) , printBinary (Y) , printf ("\n") ;
