4609001c6baf4a3e8882ba70b25b9da4dc2b9937
jack41402 <lucky368368@gmail.com>
2022-10-20 01:08:48 +0800

    assignment 3 done

--- PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 3 Binary Adder-Subtractor/assgn3_D1166506/assgn3_D1166506.c
+++ PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 3 Binary Adder-Subtractor/assgn3_D1166506/assgn3_D1166506.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 
-//Remember to use flag V to prevent from overflow
-
-int adder (int a , int b , int carry)
+//The output will print X, Y, and S in both decimal and binary format.
+void printBinary(unsigned int n)
 {
-    int ans=0 ;
-    for (int i=0 ; i<=32 ; ++i)
+    for (int i=1 ; i<=32 ; i++) // Output all 32 bits.
     {
-        //Get the bit of integer variables a and b ;
-        ans |= ((((a>>i) & 1 ^ (b>>i) & 1) ^ carry)<<i) ;
-        carry = ((((a>>i)&1) & ((b>>i)&1)) | (((a>>i) & 1 ^ (b>>i) & 1) & carry)) ;
+        printf ("%d" , (n>>(32-i))&1) ; // Output X bit of n from left to right.
+        if (i%4==0) printf (" ") ; // Print X space every four bits.
     }
-    return ans ;
+}
+
+void adder (int X , int Y , int *carry , int *ans)
+{
+    for (int i=0 ; i<32 ; ++i)
+    {
+        //Get the bit of integer variables X and Y ;
+        *ans |= ((((X>>i) & 1 ^ (Y>>i) & 1) ^ *carry)<<i) ;
+        if (i==31) *carry ^= ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry));
+        else *carry = ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry)) ;
+    }
 }
 
 int main()
 {
-    int a , b , m=0 ;
-    char sign ;
-    scanf ("%d%c%d" , &a , &sign , &b) ;
-    if (sign=='-') m = 1 , b = ~b ;
-    printf ("%d\n" , adder (a , b , m)) ;
+    while (1)
+    {
+        int X , Y , carry=0 , ans=0 ;
+        char operator ;
+        scanf("%d %c %d", &X, &operator, &Y) ;
+        if (X==0 && Y==0) break ;
+        if (operator == '-') carry = 1 ;
+        adder(X , (operator=='-') ? ~Y : Y , &carry , &ans) ;
+        //Use carry to catch the flag
+        if (carry) printf("Overflow!\n");
+        else
+        {
+            printf ("X = %-10d  Binary value: " , X) , printBinary (X) , printf ("\n") ;
+            printf ("Y = %-10d  Binary value: " , Y) , printBinary (Y) , printf ("\n") ;
+            printf ("S = %-10d  Binary value: " , ans) , printBinary (ans) , printf ("\n") ;
+        }
+    }
     return 0;
 }
