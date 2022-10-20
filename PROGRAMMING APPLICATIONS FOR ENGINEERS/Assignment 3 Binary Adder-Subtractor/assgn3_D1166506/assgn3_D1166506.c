1ee491326754ca9a94741cae3280197c3bbad720
jack41402 <lucky368368@gmail.com>
2022-10-20 23:10:14 +0800

    update assignment 3

--- PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 3 Binary Adder-Subtractor/assgn3_D1166506/assgn3_D1166506.c
+++ PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 3 Binary Adder-Subtractor/assgn3_D1166506/assgn3_D1166506.c
@@ -9,13 +9,12 @@ void printBinary(unsigned int n) // Print the integer variable in binary.
     }
 }
 
-void adder (int X , int Y , int *carry , int *ans) // full adder
+void adder (int X , int Y , int *carry , int *ans , int *overflow) // full adder
 {
     for (int i=0 ; i<32 ; ++i)
     {
         *ans |= ((((X>>i) & 1 ^ (Y>>i) & 1) ^ *carry)<<i) ; // Use Leftshift and OR to add the C_in^(X^Y) to the i-th digit.
-        if (i==31) *carry ^= ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry)); // Use XOR to catch the flag of overflow. The sum of two positive or negative numbers won't be negative or positive.
-        else *carry = ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry)) ; // To determine whether to carry in a normal situation, one calculates ((X & Y)|(XY) & carry).
+        *overflow = *carry , *carry = ((((X>>i) & 1) & ((Y>>i) & 1)) | (((X>>i) & 1 ^ (Y>>i) & 1) & *carry)) , *overflow ^= *carry ; // To determine whether to carry in a normal situation, one calculates ((X & Y)|(XY) & carry).
     }
 }
 
@@ -23,19 +22,20 @@ int main()
 {
     while (1)
     {
-        int X , Y , carry=0 , ans=0 ;
+        int X , Y , carry=0 , ans=0 , overflow ;
         char operator ;
+        printf ("Enter \"X + Y\" or \"X - Y\" (X, Y: -2,147,483,648 to 2,147,483,647): ") , fflush(stdout) ;
         scanf("%d %c %d", &X, &operator, &Y) ;
         if (X==0 && Y==0) break ; // Break the loop while X and Y are both equal to 0.
         if (operator == '-') carry = 1 ; // Carry in at the first digit while the operator is negative.
-        adder(X , (operator=='-') ? ~Y : Y , &carry , &ans) ; // Use the function adder to sum up X and Y.
-        if (carry) printf("Overflow!\n");  // Use carry to catch the flag
-        else // print the variables X, Y, and S in both decimal and binary.
-        {
-            printf ("X = %-10d  Binary value: " , X) , printBinary (X) , printf ("\n") ;
-            printf ("Y = %-10d  Binary value: " , Y) , printBinary (Y) , printf ("\n") ;
-            printf ("S = %-10d  Binary value: " , ans) , printBinary (ans) , printf ("\n") ;
-        }
+        adder(X , (operator=='-') ? ~Y : Y , &carry , &ans , &overflow) ; // Use the function adder to sum up X and Y.
+        // print the variables X, Y, and S in both decimal and binary.
+        printf ("X = %-10d  Binary value: " , X) , printBinary (X) , printf ("\n") ;
+        printf ("Y = %-10d  Binary value: " , Y) , printBinary (Y) , printf ("\n") ;
+        printf ("S = %-10d  Binary value: " , ans) , printBinary (ans) , printf ("\n") ;
+        (X+Y==ans) ? printf("Correct! Adder-subtractor operation test: %d %c %d = %d\n" , X , operator , Y , ans) : printf("Incorrect! Adder-subtractor operation test: %d %c %d = %d\n" , X , operator , Y , ans) ;
+        if (overflow) printf("Overflow!\n") ; // Use carry to catch the flag
+        fflush(stdout) ;
     }
     return 0;
 }
--- PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 3 Binary Adder-Subtractor/assgn3_D1166506/cmake-build-debug/CMakeFiles/assgn3_D1166506.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for assgn3_D1166506.
