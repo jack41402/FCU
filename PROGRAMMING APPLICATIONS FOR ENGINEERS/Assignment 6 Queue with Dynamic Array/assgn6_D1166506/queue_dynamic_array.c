27dae36b2f8f5b5f2ab1e86efbda88b2daab2bb4
jack41402 <lucky368368@gmail.com>
2022-12-16 01:26:46 +0800

    assgn6 DONE

--- PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 6 Queue with Dynamic Array/assgn6_D1166506/queue_dynamic_array.c
+++ PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 6 Queue with Dynamic Array/assgn6_D1166506/queue_dynamic_array.c
@@ -1,54 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "queue_dynamic_array.h"
 
 // Set a queue to empty, i.e., reset head and tail of a queue.
-void initial_queue(Queue *que)
+void initial_queue (Queue *que)
 {
+    que->elem = (int *) calloc (SEGMENT , sizeof (int)) ;
     que->head = 0 ; // Head index is where the next element will be deleted if not empty.
     que->tail = 0 ; // Tail index is where the next element will be placed.
-    que->capacity = 0 ; // When capacity==0, the queue is empty.
+    que->capacity = SEGMENT ; // When capacity==0, the queue is empty.
 }
 
 // Add an element to the tail of a queue.
 // The elements are placed in the queue circularly.
-void enqueue(Queue *que , int e)
+void enqueue (Queue *que , int e)
 {
     // Increase the capacity if the queue is full.
-    printf ("%d %d\n" , que->tail , que->capacity) ;
-    if (que->tail>que->capacity)
+    if (get_size (*que)+1==que->capacity)
     {
         que->capacity += SEGMENT ;
-        que->elem = (int *) calloc (que->capacity , sizeof (int)) ;
+        que->elem = (int *) realloc (que->elem , que->capacity * sizeof (int)) ;
     }
     // If the queue is not full.
     *(que->elem+que->tail) = e ; // Place the element at the tail.
-//    printf ("%d %d\n" , *(que->elem+que->tail) , que->tail) ;
-    que->tail++ ; // Increment tail index.
+    que->tail++ ;
 }
 
 // Remove an element from the head of a queue.
 // The elements are placed in the queue circularly.
-int dequeue(Queue *que)
+int dequeue (Queue *que)
 {
     // FREE THE MEMORY!!!
     int e ; // Head element.
-    if (que->capacity>0)
+    if (!is_empty (*que))
     {
-        e = que->elem[que->head] ; // Get the element at the head of queue.
-        que->head = que->head + 1 ; // Increment head index.
-        que->capacity-- ; // Decrement the number of elements.
-        return e; // Return the head element.
+        e = *(que->elem+que->head++) ; // Get the element at the head of queue. Increment head index.
     }
-    else
+    if (que->capacity-SEGMENT>get_size (*que))
     {
-        printf("\n  Queue underflow.\n") ; // The queue is underflow.
-        return -9999; // Value -9999 indicates underflow.
+        que->capacity -= SEGMENT ;
+        que->elem = (int *) realloc (que->elem , que->capacity * sizeof (int)) ;
     }
+    return e; // Return the head element.
+
+}
+
+void reset (Queue *que)
+{
+    Queue temp=*que ;
+    int count=0 ;
+    for (int i=que->head ; i<que->tail ; ++i , ++count)
+    { // Print all elements in queue.
+        *(que->elem+count) = *(temp.elem+i%get_size (temp)) ; // Print an element.
+//        printf ("Compare: %3d %3d\n" , *(que->elem+count) , *(temp.elem+i%get_size (temp))) ;
+    }
+    que->head=0 , que->tail = count ;
+//    free (temp.elem) ;
 }
 
 // Get the element at the head of a queue.
-int head(Queue que)
+int head (Queue que)
 {
     if (que.capacity>0) return que.elem[que.head] ; // If queue is not empty, return the head element.
     else
@@ -59,15 +71,15 @@ int head(Queue que)
 }
 
 // Check if a queue is empty or not.
-int is_empty(Queue que)
+int is_empty (Queue que)
 {
-    return (que.capacity==0); // capacity==0 means empty queue.
+    return (get_size (que)==0); // size==0 means empty queue.
 }
 
 // Get size of queue.
-int get_size(Queue que)
+int get_size (Queue que)
 {
-    return que.tail-que.head-1;
+    return que.tail-que.head;
 }
 
 // Clear queue and set capacity to one segment.
@@ -77,14 +89,14 @@ int get_size(Queue que)
 //}
 
 // Print elements of a queue from the head to the tail.
-void print_queue(Queue que)
+void print_queue (Queue que)
 {
     if (que.capacity>0)
     {
         printf("****Queue elements from head to tail:\n") ;
-        for (int i=que.head+1 ; i<que.tail ; i++)
+        for (int i=que.head ; i<que.tail ; i++)
         { // Print all elements in queue.
-            printf(" %d" , *(que.elem+i)) ; // Print an element.
+            printf("%4d" , *(que.elem+i%get_size (que))) ; // Print an element.
         }
         printf("\n") ;
     }
--- PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 6 Queue with Dynamic Array/assgn6_D1166506/queue_dynamic_array_main.c
+++ PROGRAMMING APPLICATIONS FOR ENGINEERS/Assignment 6 Queue with Dynamic Array/assgn6_D1166506/queue_dynamic_array_main.c
@@ -12,69 +12,29 @@ int main(void)
 
     initial_queue(&Q) ; // Initial queue q.
     trail_count = rand()%10 + 1 ;
+    trail_count = 10 ;
     for (int i=1 ; i<=trail_count ; ++i)
     {
-        printf (">>>> Trail%d:\n" , i) ;
+        printf (">>>> Trial %d:\n" , i) ;
         enqueue_count = rand()%100 + 1 ;
-        printf ("Insert %d elements to queue Q. Content of queue Q after inserting elements:\n") ;
+        printf ("Insert %d elements to queue Q. Content of queue Q after inserting elements:\n", enqueue_count) ;
         while (enqueue_count--)
         {
             e = rand()%100 + 1 ;
-//            printf ("%d\n" , e) ;
             enqueue (&Q , e) ;
         }
         printf ("Queue capacity: %d\n" , Q.capacity) ;
         printf ("Queue size    : %d\n" , get_size (Q)) ;
         print_queue (Q) ;
-        dequeue_count = rand() % Q.capacity + 1 ;
-
+        dequeue_count = rand() %get_size (Q) + 1 ;
+        printf ("\nRemove %d elements to queue Q. Content of queue Q after removing elements:\n" , dequeue_count) ;
+        while (dequeue_count--) dequeue (&Q) ;
+        reset (&Q) ;
+        printf ("Queue capacity: %d\n" , Q.capacity) ;
+        printf ("Queue size    : %d\n" , get_size (Q)) ;
+        print_queue (Q) ;
+        printf ("-------------------------------------------------------------------\n") ;
     }
-//    while (1)
-//    { // Repeat the command until quit
-//        // Command list.
-//        printf("1. Enqueue     2. Dequeue      3. Head element\n") ;
-//        printf("4. Empty test  5. Print queue  6. Quit\n") ;
-//        printf("**** Enter a command: ") ;
-//        scanf("%d", &cmd) ; // Read a command.
-//        switch (cmd)
-//        {
-//            case 1:
-//            { // 1. Enqueue.
-//                printf("\nEnqueue operation.\n  Enter an enqueued element: ") ;
-//                scanf("%d", &e) ;
-//                enqueue(&q, e) ; // Enqueue.
-//                break ;
-//            }
-//            case 2:
-//            { // 2. Dequeue.
-//                printf("\nDequeue operation.\n") ;
-//                e = dequeue(&q) ; // Dequeue.
-//                if (e!=-9999) printf("\n  Element %d has been dequeued.\n", e) ;
-//                break ;
-//            }
-//            case 3:
-//            { // 3. Head element.
-//                e = head(q) ; // Get head element.
-//                if (e!=-9999) printf("\n  The head element is %d.\n", e) ;
-//                break ;
-//            }
-//            case 4:
-//            { // 4. Empty test.
-//                if (is_empty(q)) printf("\n  The queue is empty.\n") ;
-//                else printf("\n  The queue is not empty.\n") ;
-//                break ;
-//            }
-//            case 5: { // 5. Print queue.
-//                print_queue(q) ;
-//                break ;
-//            }
-//            case 6: { // 6. Quit.
-//                return 0 ; // Return from the main program.
-//            }
-//            default: { // Incorrect command.
-//                printf("Incorrect command.\n") ;
-//            }
-//        } ;
-//        printf("\n****************************************************\n\n") ;
-//    }
+    free (Q.elem) ;
+    return 0;
 }
