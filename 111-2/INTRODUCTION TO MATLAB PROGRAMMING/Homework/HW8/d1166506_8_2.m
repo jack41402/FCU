0e21ffcf571eb892e71896b4c6da6dc37a486920
jack41402 <lucky368368@gmail.com>
2023-04-13 02:01:44 +0800

    Update d1166506_8_2.m

--- 111-2/INTRODUCTION TO MATLAB PROGRAMMING/Homework/HW8/d1166506_8_2.m
+++ 111-2/INTRODUCTION TO MATLAB PROGRAMMING/Homework/HW8/d1166506_8_2.m
@@ -1,8 +1,8 @@
-f = @(x) 1 + exp(-0.2*x)*sin(x+2) ;
+f = @(x) 1 + exp(-0.2.*x).*sin(x+2) ;
 
 % (a) Find the minimum value for the function
 [x , y] = fminbnd(f , 0 , 10) ;
-disp([x y])
+disp([x , y])
 
 % (b) Use fplot to plot this function
 fplot(f , [0 10])
@@ -10,7 +10,7 @@ hold on
 
 % (c) c=2.5
 c = 2.5 ;
-f = @(x) 1 + exp(-0.2*x)*sin(x+c) ;
+f = @(x) 1 + exp(-0.2.*x).*sin(x+c) ;
 [x , y] = fminbnd(f , 0 , 10) ;
 disp([x y])
 fplot(f , [0 10])
