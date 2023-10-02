d6c2bbdda0ac7c809e4aa5e5ead4ddbdde3aa05b
jack41402 <lucky368368@gmail.com>
2023-10-03 00:44:57 +0800

    Update Web Programming GUI done

--- 112-1/WEB PROGRAMMING/Lab/Lab 3 – Simple Mail Client/d1166506/TEST.py
+++ 112-1/WEB PROGRAMMING/Lab/Lab 3 – Simple Mail Client/d1166506/TEST.py
@@ -14,7 +14,7 @@ class MailboxApp(QMainWindow):
         self.setWindowTitle('Mailbox')
         self.setGeometry(100, 100, 1000, 600)
 
-        # Create mail table
+        # 创建邮件表格
         self.mail_table = QTableWidget(self)
         self.mail_table.setShowGrid(False)
         self.mail_table.setSortingEnabled(True)
@@ -22,7 +22,7 @@ class MailboxApp(QMainWindow):
         self.mail_table.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
         self.mail_table.doubleClicked.connect(self.show_mail_content)
 
-        # Hide horizontal header, and only show vertical header
+        # 隐藏水平标头，只显示垂直标头
         header = QHeaderView(Qt.Orientation.Horizontal, self.mail_table)
         self.mail_table.setHorizontalHeader(header)
         header.setSectionsClickable(True)  # 允许点击表头
@@ -30,39 +30,39 @@ class MailboxApp(QMainWindow):
         self.mail_table.setColumnCount(5)
         self.mail_table.setHorizontalHeaderLabels(['', 'From', 'Subject', 'Date', 'Time'])
 
-        # Create text browser to show mail content
+        # 创建右侧显示邮件内容的文本框
         self.mail_content = QTextBrowser(self)
         self.mail_content.setReadOnly(True)
 
-        # Create select all checkbox
+        # 创建底部的选择框
         self.select_all_checkbox = QCheckBox('Select All', self)
         self.select_all_checkbox.setChecked(False)  # 设置"Select All"复选框初始状态为不选中
 
-        # Create horizontal layout to place checkbox
+        # 创建一个水平布局用于放置选择框和表格
         checkbox_layout = QHBoxLayout()
         checkbox_layout.addWidget(self.select_all_checkbox)
 
-        # Create splitter between mail table and mail content
+        # 创建可调整大小的分隔线
         splitter = QSplitter(Qt.Orientation.Horizontal)
         splitter.addWidget(self.mail_table)
         splitter.addWidget(self.mail_content)
 
-        # Create horizontal layout to place mail table
+        # 创建一个垂直布局用于放置选择框、表格和分隔线
         main_layout = QVBoxLayout()
         main_layout.addLayout(checkbox_layout)
         main_layout.addWidget(splitter)
 
-        # Create central widget as main layout
+        # 创建一个容器部件，将垂直布局设置为主窗口的中心部件
         central_widget = QWidget()
         central_widget.setLayout(main_layout)
         self.setCentralWidget(central_widget)
 
-        # Add example mails
+        # 添加一些示例邮件
         self.add_mail('Sender 1', 'Subject 1', 'Date 1', 'Time 1', 'Content 1')
         self.add_mail('Sender 2', 'Subject 2', 'Date 2', 'Time 2', 'Content 2')
         self.add_mail('Sender 3', 'Subject 3', 'Date 3', 'Time 3', 'Content 3')
 
-        # Adjust checkbox width
+        # 设置选择框列宽度较小
         self.mail_table.setColumnWidth(0, 20)
 
         # 连接"Select All"复选框的状态改变信号
