d6f25f89ef0898cd735d15542146b46ed96cce0b
Jack <lucky368368@gmail.com>
2024-04-18 02:06:16 +0800

    Update

--- 112-2/INTRODUCTION TO MATLAB PROGRAMMING/Midterm/midterm_4.m
+++ 112-2/INTRODUCTION TO MATLAB PROGRAMMING/Midterm/midterm_4.m
@@ -1,7 +1,8 @@
-format rat
+format default
+
 a = 2;
 
 fprintf("\tn\t\tx_n\n");
 for n=0:20
-    fprintf("\t%d\t%-10s\n", n, rats(func(a, n), 10));
+    fprintf("\t%d\t%-10s\n", n, rats(func(a, n), 20));
 end
