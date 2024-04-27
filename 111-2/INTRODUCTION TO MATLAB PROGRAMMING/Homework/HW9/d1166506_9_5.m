4076d3fb4b85d3988b8a8530028feccb35ff0f93
Jack <lucky368368@gmail.com>
2024-04-27 19:33:52 +0800

    Update

--- 111-2/INTRODUCTION TO MATLAB PROGRAMMING/Homework/HW9/d1166506_9_5.m
+++ 111-2/INTRODUCTION TO MATLAB PROGRAMMING/Homework/HW9/d1166506_9_5.m
@@ -3,9 +3,16 @@ rad = deg2rad(50) ;
 x = @(t) v*cos(rad).*t ;
 y = @(t) v*sin(rad).*t - 0.5*9.81.*t.^2 ;
 t = 0 ;
-fprintf("time horizontal distance vertical distance\n") ;
+fprintf("\ttime      x           y\n") ;
+
 while y(t)>=0
-    disp([t x(t)     y(t)])
+    time_str = sprintf('%.1f', t);
+    hdist_str = sprintf('%3.4f', x(t));
+    vdist_str = sprintf('%3.4f', y(t));
+    fprintf("\t%s\t%+10s\t%+10s\n", time_str, hdist_str, vdist_str)
+    % disp(['    ', time_str, '    ', hdist_str, '    ', vdist_str]); % Concatenate the strings with proper spacing
+    % fprintf("\t%.1f   \t%3.4f   %3.4f\n", t, x(t), y(t))
+    % disp([t x(t) y(t)])
     t = t + 0.5 ;
 end
 
